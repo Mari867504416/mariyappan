838dff62da007dc7e8425a34f54ba05a88afcab8
Mari867504416 <mariyappan9600@gmail.com>
2025-04-15 09:14:51 -0700

    Update officerControler.js

--- controllers/officerControler.js
+++ controllers/officerControler.js
@@ -1,4 +1,4 @@
-const Officer = require('./models/Officer');
+const Officer = require('../models/Officer');
 const AppError = require('../utils/appError');
 
 exports.activateOfficer = async (req, res, next) => {
