2c2385056b4c08ac1cbe0a9ce38e8580b880f5ae
Mari867504416 <mariyappan9600@gmail.com>
2025-04-15 10:11:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const connectDB = require('./config/db');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan'); // ONLY ONCE
 const app = express();
+
 
 // Middleware
 app.use(morgan('dev'));
