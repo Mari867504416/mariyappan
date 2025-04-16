c196dc51fbfa8d3eb0a1096d2ebce6b0d1b85414
Mari867504416 <mariyappan9600@gmail.com>
2025-04-15 19:52:11 -0700

    Update officerRoutes.js

--- routes/officerRoutes.js
+++ routes/officerRoutes.js
@@ -11,3 +11,9 @@ router.post('/activate', officerController.activateOfficer);
 router.get('/me', officerController.getOfficerProfile);
 
 module.exports = router;
+router.get('/debug', (req, res) => {
+  res.json({
+    controllerPath: path.resolve(__dirname, '../controllers/officerController.js'),
+    exists: require('fs').existsSync(path.resolve(__dirname, '../controllers/officerController.js'))
+  });
+});
